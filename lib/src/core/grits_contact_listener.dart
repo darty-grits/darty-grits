c86e4bdec0f21335578da1891372c8c67ff5593f
Yulian Kuncheff <yulian@kuncheff.com>
2013-03-16 14:34:53 -0700

    Physics Engine, Contact Listener, Pubspec

---
+++ lib/src/core/grits_contact_listener.dart
@@ -0,0 +1,16 @@
+part of physics_engine;
+
+class GritsContactListener implements ContactListener {
+
+  void beginContact(Contact contact) {
+  }
+
+  void endContact(Contact contact) {
+  }
+
+  void preSolve(Contact contact, Manifold oldManifold) {
+  }
+
+  void postSolve(Contact contact, ContactImpulse impulse) {
+  }
+}
