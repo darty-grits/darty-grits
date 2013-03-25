fe333d07446c337c84f112e396f8fe376d3a4fd8
adam <financeCoding@gmail.com>
2013-03-24 21:02:59 -0700

    Adding items

--- lib/src/items/energy_canister.dart
+++ lib/src/items/energy_canister.dart
@@ -1,5 +1,65 @@
 part of grits_shared;
 
 class EnergyCanister extends Entity {
+  var physBody;
+  
+  EnergyCanister(x, y, settings) /*: super(x, y, settings)*/ {
+    var startPos = {'x':x,'y':y}; // TODO: change to vec2
+    var guid = newGuid_short();
+    //create our physics body;
+    var entityDef = {
+      'id': "EnergyCanister${guid}",
+      'type': 'static',
+      'x': startPos['x'],
+      'y': startPos['y'],
+      'halfHeight': 18 * 0.5,
+      'halfWidth': 19 * 0.5,
+      'damping': 0,
+      'angle': 0,
+      'categories': ['projectile'],
+      'collidesWith': ['player'],
+      'userData': {
+        "id": "EnergyCanister${guid}",
+        "ent": this
+      }
+    };
+    
+    physBody = gPhysicsEngine.addBody(entityDef);
 
+    physBody.SetLinearVelocity(new vec2(0, 0));
+  }
+  
+  kill() {
+    //remove my physics body
+    gPhysicsEngine.removeBodyAsObj(physBody);
+    physBody = null;
+    //destroy me as an ent.
+    gGameEngine.removeEntity(this);
+  }
+  
+  onTouch(otherBody, point, impulse) {
+    if (physBody == null) {
+      return false;
+    }
+
+    if (otherBody.GetUserData() == null) {
+      return false; //invalid object??
+    }
+    
+    var physOwner = otherBody.GetUserData().ent;
+    
+    if (physOwner != null) {
+      if (physOwner._killed) {
+        return false;
+      }
+      
+      if (physOwner.energy < physOwner.maxEnergy) {
+        physOwner.energy = Math.min(physOwner.maxEnergy, physOwner.energy + 10);
+      }
+        
+      this.markForDeath = true; 
+    }
+
+    return true; //return false if we don't validate the collision
+  }
 }
--- lib/src/items/health_canister.dart
+++ lib/src/items/health_canister.dart
@@ -1,5 +1,65 @@
 part of grits_shared;
 
 class HealthCanister extends Entity {
+  var physBody;
+  
+  HealthCanister(x, y, settings) /* : super(x, y, settings) */ {
+    var startPos = {'x':x,'y':y};
+    var guid = newGuid_short();
+    //create our physics body;
+    var entityDef = {
+      'id': "HealthCanister${guid}",
+      'type': 'static',
+      'x': startPos['x'],
+      'y': startPos['y'],
+      'halfHeight': 18 * 0.5,
+      'halfWidth': 19 * 0.5,
+      'damping': 0,
+      'angle': 0,
+      'categories': ['projectile'],
+      'collidesWith': ['player'],
+      'userData': {
+        "id": "HealthCanister${guid}",
+        "ent": this
+      }
+    }; 
+    
+    physBody = gPhysicsEngine.addBody(entityDef);
 
+    physBody.SetLinearVelocity(new vec2(0, 0));
+  }
+  
+  kill() {
+    //remove my physics body
+    gPhysicsEngine.removeBodyAsObj(physBody);
+    physBody = null;
+    //destroy me as an ent.
+    gGameEngine.removeEntity(this);
+  }
+  
+  onTouch(otherBody, point, impulse) {
+    if (this.physBody == null) {
+      return false;
+    }
+
+    if (otherBody.GetUserData() == null) {
+      return false; //invalid object??
+    }
+    
+    var physOwner = otherBody.GetUserData().ent;
+    
+    if (physOwner != null) {
+      if (physOwner._killed) {
+        return false;
+      }
+      
+      if (physOwner.Health < physOwner.maxHealth) { 
+        physOwner.Health = Math.min(physOwner.maxHealth, physOwner.health + 10);
+      }
+        
+      this.markForDeath = true;
+    }
+
+    return true; //return false if we don't validate the collision
+  }
 }
